65da9c7b36e3648fbc68d278e0dc06493176eb84
anfeifan <anfeifan@superatomfin.com>
2022-10-07 21:23:13 +0800

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 0.0.1
+
+* Modify from https://pub.dev/packages/qr_code_scanner  
+* 修改使用 添加错误显示 ui修改 android minSdk 修改
---
+++ lib/src/web/flutter_qr_stub.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import '../../qr_code_scan_pay.dart';
+
+Widget createWebQrView(
+        {onPlatformViewCreated, onPermissionSet, CameraFacing? cameraFacing}) =>
+    const SizedBox();
